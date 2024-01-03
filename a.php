f7d610c4a5fac242582e8745e7386be0d82c358f
wasinbudprom <wasin.budprom@hotmail.com>
2024-01-03 13:43:54 +0700

    example1 in php
    example1 in php

---
+++ a.php
@@ -0,0 +1,11 @@
+<?php
+
+    $a=4;
+    $b=10;
+        echo "Result 1 = " . ($a + $b) . "<br>";
+        echo "Result 2 = " . ($b - $a) . "<br>";
+        echo "Result 3 = " . ($a * $b) . "<br>";
+        echo "Result 4 = " . ($b / $a) . "<br>";
+
+
+?>
