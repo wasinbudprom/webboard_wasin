608b8894561bbda84ee2b580bab9dbc47adca87f
wasinbudprom <128619089+wasinbudprom@users.noreply.github.com>
2024-03-18 01:23:32 +0700

    export sql 18/3/2567

--- webboard.sql
+++ webboard.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2024 at 11:09 AM
+-- Generation Time: Mar 17, 2024 at 07:22 PM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -83,7 +83,9 @@ CREATE TABLE `post` (
 --
 
 INSERT INTO `post` (`id`, `title`, `content`, `post_date`, `cat_id`, `user_id`) VALUES
-(1, 'web', 'asd', '2024-02-28 16:46:55', 1, 8);
+(1, 'web', 'asd', '2024-02-28 16:46:55', 1, 8),
+(2, 'study', 'study', '2024-03-17 18:02:03', 2, 7),
+(3, 'Sport', 'Sport\r\n', '2024-03-17 19:25:22', 3, 7);
 
 -- --------------------------------------------------------
 
@@ -106,14 +108,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `login`, `password`, `name`, `gender`, `email`, `role`) VALUES
-(1, 'test1', '123456', 'test 1', 'm', 'test@hotmail.com', 'm'),
-(2, 'test2', '123', 'test2', 'm', 'test2@hotmail.com', 'm'),
-(3, 'test3', '123', 'test3', 'm', 'test3@hotmail.com', 'm'),
-(4, 'test4', '123', 'test4 ', 'f', 'test4@hotmail.com', 'm'),
-(5, 'test5', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'test5123', 'o', 'test5@hotmail.com', 'm'),
-(6, 'qwe', '056eafe7cf52220de2df36845b8ed170c67e23e3', 'qwe', 'f', 'qwe@hotmail.com', 'm'),
-(7, 'wasin', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'wasin', 'f', 'wasin@hotmail.com', 'm'),
-(8, 'wasin1', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'asd', 'm', 'asd@hotmail.com', 'm');
+(7, 'wasin', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'wasin', 'f', 'wasin@hotmail.com', 'a'),
+(8, 'wasin1', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'asd', 'm', 'asd@hotmail.com', 'm'),
+(10, 'bobo', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'bobo', 'f', 'bobo@123', 'm');
 
 --
 -- Indexes for dumped tables
@@ -163,13 +160,13 @@ ALTER TABLE `comment`
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
